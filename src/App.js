a29ce61db3c0a14fde05a80dcd080f9b94697fc6
mayhixza <emailwithmoi@gmail.com>
2021-05-19 17:15:15 +0530

    new message

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     userName={localStorage.getItem('username')}
     userSecret={localStorage.getItem('password')}
     renderChatFeed = {(chatAppProps) => <ChatFeed {... chatAppProps}/>}
+    onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
   />
   )
 }
